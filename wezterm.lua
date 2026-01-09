bed224300c18c546d8cccb403a6bc9e430a047ff
Treyvon Whitaker <ixione.dev@gmail.com>
2026-01-08 20:47:55 -0600

    chore(config): add reactive colorscheme

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,14 @@ config.initial_rows = 24
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 12
-config.color_scheme = "tokyonight_night"
+
+local now = os.date("*t", os.time())
+local frac_hour = now.hour + (now.min / 60) + (now.sec / 3600)
+if frac_hour >= 7 and frac_hour < 19 then
+	config.color_scheme = "tokyonight_day"
+else
+	config.color_scheme = "tokyonight_night"
+end
 
 config.window_background_opacity = 0.8
 
